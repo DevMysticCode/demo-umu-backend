5da811b14354ef158f93ea57e8772dee341346ed
Bunty1998 <pradeep.yadav.ick@gmail.com>
2026-02-09 12:52:10 +0530

    CORS update for deployment

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3002',
+      'https://demo-umu-frontend.vercel.app/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
